7f4137ac95685eaa793aeb2c688e4119ec905652
J24RM <rodriguezmendozajesus8@gmail.com>
2024-10-04 17:34:46 -0600

    Juego_Ahorcado.py

--- Juego_Ahorcado.py
+++ Juego_Ahorcado.py
@@ -1,81 +1,148 @@
+import random
 # Esta función se encarga de imprimir cuantas letras tiene la palabra en forma de -
 def Generador():
-    Palabra: "Azul"
-    NumPalabra = 4
-    
-    for i in range (1, NumPalabra+1):
-        if i == NumPalabra:
-            print("-")
-        else:
-            print("-", end = " ")
+  Nivel1 = ["Azul","Rojo","Verde"]
+  Nivel2 = []
+  Nivel3 = []
+
+  Respuesta = []
+  CorrectoComparar = []
+
+  Pelejida = random.randint(0,len(Nivel1)-1)
+  for i in range (len(Nivel1[Pelejida])):
+    Respuesta.append("-")
+  for j in range (len(Nivel1[Pelejida])):
+    CorrectoComparar.append(Nivel1[Pelejida][j])
+
+  return Nivel1[Pelejida],Respuesta,CorrectoComparar
 
 # Esta función se encarga de comprobar que tu letra ingresada este o no este en la palabra
-def actualizar(letra,x1,x2,x3,x4,Vidas):
-    Palabra: "Azul"
-    NumPalabra = 4
-    O = 0
-    P = 4
-    for i in range(1,NumPalabra + 1):
-        if i == 1:
-            if letra == "a" or x1 == 1 or letra == "A":
-                print("A", end = " ")
-                if x1 != 1:
-                    O = O + 1
-                x1 = 1
-            else:
-                print("-", end = " ")
-        elif i == 2:
-            if letra == "z" or x2 == 1:
-                print("z", end = " ")
-                if x2 != 1:
-                    O = O + 1
-                x2 = 1
-            else:
-                print("-", end = " ")
-        elif i == 3:
-            if letra == "u" or x3 == 1:
-                print("u", end = " ")
-                if x3 != 1:
-                    O = O + 1
-                x3 = 1
-            else:
-                print("-", end = " ")
-        elif i == 4:
-            if letra == "l" or x4 == 1:
-                print("l")
-                if x4 != 1:
-                    O = O + 1
-                x4 = 1
-            else:
-                print("-")
-                
-                
-    if O == 0:
-        Vidas = Vidas - 1
-                
-    return x1,x2,x3,x4,Vidas
-                    
-# Aqui es dpnde se lleva todo el juego, aqui se pregunta la letra y te dice si perdiste o ganaste 
+def actualizar(letra,Vidas,VidasD,Respuesta,Correcto):
+  QuitarVida = 0
+  for i in range (len(Correcto)):
+    if letra == Correcto[i]:
+      Respuesta[i] = letra
+      QuitarVida = 1
+  if QuitarVida == 0:
+    Vidas = Vidas - 1
+    VidasD = VidasD + 1
+  return Vidas,VidasD,Respuesta
+
+# Esta función se encarga de imprimir el ahorcado
+def Dibujo(VidaD):
+  Dibuj=["""
+   +---+
+   |   |
+
+       |
+
+       |
+
+       |
+
+       |
+
+========= """ , """
+
+   +---+
+
+   |   |
+
+   O   |
+
+       |
+
+       |
+
+       |
+
+========= """ , """
+   +---+
+
+   |   |
+
+   O   |
+
+   |   |
+
+       |
+
+       |
+
+========= """ , """
+   +---+
+
+   |   |
+
+   O   |
+
+  /|   |
+
+       |
+
+       |
+
+========= """ , """
+   +---+
+
+   |   |
+
+   O   |
+
+  /|\  |
+
+       |
+
+       |
+
+========= """ , """
+   +---+
+
+   |   |
+
+   O   |
+
+  /|\  |
+
+  /    |
+
+       |
+========= """ , """
+   +---+
+
+   |   |
+
+   O   |
+
+  /|\  |
+
+  / \  |
+
+       |
+
+========= """]
+
+  print(Dibuj[VidaD])
+# Aqui es dpnde se lleva todo el juego, aqui se pregunta la letra y te dice si perdiste o ganaste
 def Main():
-    x1 = 0
-    x2 = 0
-    x3 = 0
-    x4 = 0
     Vidas = 6
-    Generador() 
+    VidasD = 0
+    Correcto,Respuesta,CorrectoComparar = Generador()
     while True:
+        Dibujo(VidasD)
+        print(Respuesta)
         print("numero de vidas", Vidas)
         letra = input("Ingresa: ")
-        x1,x2,x3,x4,Vidas = actualizar(letra,x1,x2,x3,x4,Vidas)
-        
+        Vidas,VidasD,Respuesta = actualizar(letra,Vidas,VidasD,Respuesta,Correcto)
+
         if Vidas == 0:
             print("Perdiste")
             break
-        elif x1 == 1 and x2 == 1 and x3 == 1 and x4 == 1:
+        elif CorrectoComparar == Respuesta:
             print("Ganaste")
             break
-        
-        
+
+
 """
 Input 1: a
 Respuesta: a---
@@ -103,34 +170,57 @@ Vidas:4
 Ganaste
 """
 def Prueba():
-    x1 = 0
-    x2 = 0
-    x3 = 0
-    x4 = 0
+
     Vidas = 6
-    Generador()
-    
-    R1 = "a"
+    VidasD = 0
+
+    Correcto = "azul"
+    CorrectoComparar = ["a","z","u","l"]
+    Respuesta = ["-","-","-","-"]
+
+    Dibujo(VidasD)
+    print(Respuesta)
     print("numero de vidas", Vidas)
-    x1,x2,x3,x4,Vidas = actualizar(R1,x1,x2,x3,x4,Vidas)
-    R1 = "l"
+
+    letra = "a"
+    Vidas,VidasD,Respuesta = actualizar(letra,Vidas,VidasD,Respuesta,Correcto) 
+    Dibujo(VidasD)
+    print(Respuesta)
     print("numero de vidas", Vidas)
-    x1,x2,x3,x4,Vidas = actualizar(R1,x1,x2,x3,x4,Vidas)
-    R1 = "z"
+
+    letra = "l"
+    Vidas,VidasD,Respuesta = actualizar(letra,Vidas,VidasD,Respuesta,Correcto) 
+    Dibujo(VidasD)
+    print(Respuesta)
     print("numero de vidas", Vidas)
-    x1,x2,x3,x4,Vidas = actualizar(R1,x1,x2,x3,x4,Vidas)
-    R1 = "q"
+
+    letra = "z"
+    Vidas,VidasD,Respuesta = actualizar(letra,Vidas,VidasD,Respuesta,Correcto) 
+    Dibujo(VidasD)
+    print(Respuesta)
     print("numero de vidas", Vidas)
-    x1,x2,x3,x4,Vidas = actualizar(R1,x1,x2,x3,x4,Vidas)
-    R1 = "x"
+
+    letra = "q"
+    Vidas,VidasD,Respuesta = actualizar(letra,Vidas,VidasD,Respuesta,Correcto) 
+    Dibujo(VidasD)
+    print(Respuesta)
     print("numero de vidas", Vidas)
-    x1,x2,x3,x4,Vidas = actualizar(R1,x1,x2,x3,x4,Vidas)
-    R1 = "u"
+
+    letra = "x"
+    Vidas,VidasD,Respuesta = actualizar(letra,Vidas,VidasD,Respuesta,Correcto) 
+    Dibujo(VidasD)
+    print(Respuesta)
     print("numero de vidas", Vidas)
-    x1,x2,x3,x4,Vidas = actualizar(R1,x1,x2,x3,x4,Vidas)
+
+    letra = "u"
+    Vidas,VidasD,Respuesta = actualizar(letra,Vidas,VidasD,Respuesta,Correcto) 
+    Dibujo(VidasD)
+    print(Respuesta)
+    print("numero de vidas", Vidas)
+
     if Vidas == 0:
-            print("Perdiste")
-    elif x1 == 1 and x2 == 1 and x3 == 1 and x4 == 1:
-        print("Ganaste")
-    
+      print("Perdiste")
+    elif CorrectoComparar == Respuesta:
+      print("Ganaste")
+
 Main()
